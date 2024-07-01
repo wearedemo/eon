42771b905311ae833127d87cbedadcb454e7abfa
v-khdumi <30616704+v-khdumi@users.noreply.github.com>
2024-07-02 01:55:36 +0300

    Update button.tsx

--- src/features/ui/button.tsx
+++ src/features/ui/button.tsx
@@ -8,15 +8,15 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-[#fb5f11] text-primary-foreground hover:bg-[#fb5f11]/90",
         destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+          "bg-[#fb5f11] text-destructive-foreground hover:bg-[#fb5f11]/90",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+          "bg-[#fb5f11] text-secondary-foreground hover:bg-[#fb5f11]/80",
+        ghost: "hover:bg-[#fb5f11] hover:text-accent-foreground",
+        link: "text-[#fb5f11] underline-offset-4 hover:underline",
       },
       size: {
         default: "h-10 px-4 py-2",
